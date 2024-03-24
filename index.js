daac6c80dad6a29323f531f52237345775540aa0
sahil singh <ss2202002@gmail.com>
2024-03-24 18:47:22 +0530

    changes

--- index.js
+++ index.js
@@ -35,8 +35,18 @@ app.get('/api/:date', (req, res) => {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api', (req, res) => {
+  let dateString = new Date();
+  if (dateString === "") {
+    dateString = new Date();
+  }
+  const dateObj = new Date(dateString);
+  if (isNaN(dateObj.getTime())) {
+    return res.status(400).json({ error: 'Invalid Date' });
+  }
+  const unixTimestamp = dateObj.getTime();
+  const utcString = dateObj.toUTCString();
+  res.json({ unix: unixTimestamp, utc: utcString });
 });
 
 
